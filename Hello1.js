994cca64238923b363367fbb969457de96132999
ankur-saxena-88 <indiarocks88@gmail.com>
2022-04-05 00:01:06 +0530

    ECMAScript 2015 (ES6) Hello World program added

---
+++ Hello1.js
@@ -0,0 +1,13 @@
+/*
+ * ECMAScript 2015 Hello World Program.
+ * Date: Mon, 04-Apr-2022
+ * @author: Ankur Saxena
+ * Platform: Linux Ubuntu 20.04/LTS/x64/Nodejs 10.19.0/VIM Editor
+ */
+ 
+// program start
+
+console.log ("Hello, ECMAScript 2015 World!")
+
+// program exit
+
