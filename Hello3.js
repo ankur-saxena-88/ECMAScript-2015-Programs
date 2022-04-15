bd4242838f8662a9dc7951b65bf41773a03a23bd
ankur-saxena-88 <indiarocks88@gmail.com>
2022-04-15 17:14:17 +0530

    Hello3.js added

---
+++ Hello3.js
@@ -0,0 +1,18 @@
+/*
+Program: ES6 program to print the following multiline string on the console screen.
+
+Hello, ES6 World!
+Welcome to the ES6 programming!!
+Let us learn ES6 programming language.
+
+Date: Fri, 15-04-2022
+@author: Ankur Saxena
+Platform: Linux Ubuntu 20.04/LTS/x64/Nodejs 10.19.0/Vim editor
+*/
+
+// program start
+console.log ("Hello, ES6 World!")
+console.log ("Welcome to the ES6 programming!!")
+console.log ("Let us learn ES6 programming language.")
+// program exit
+
