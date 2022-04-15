750bc3eb5842d26770d3496867288a6e60016487
ankur-saxena-88 <indiarocks88@gmail.com>
2022-04-15 17:12:26 +0530

    Hello2.js added

---
+++ Hello2.js
@@ -0,0 +1,15 @@
+/*
+ * Program: ES6 program to print your name, date of birth, mobile no, email and address on the console screen.
+ * Date: Fri, 15-04-2022
+ * @author: Ankur Saxena
+ * Platform: Linux Ubuntu 20.04/LTS/x64/Nodejs 10.19.0/Vim editor
+ */
+
+// program start
+console.log ("Name: Ankur Saxena")
+console.log ("DOB: 04 June, 1988")
+console.log ("Mobile: +91-8905738832")
+console.log ("Email: author88ankur@hotmail.com")
+console.log ("Address: CO Shri. Arvind Saxena\n\t71/79, Shri. Govind Kutir\n\tSanganer, Pratap Nagar\n\tJaipur, Rajasthan\n\tIndia")
+// program exit
+
