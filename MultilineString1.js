d688332c288840ce8a0aae6df5079c9260022f44
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-04-17 17:36:47 +0530

    MultilineString1.js added

---
+++ MultilineString1.js
@@ -0,0 +1,12 @@
+/*
+Program: Write a ES6 program to print following multiline string on the console screen using new line '\n' escape sequence.
+Date: Sun, 17-4-2022
+@author: Ankur Saxena
+Platform: Linux Ubuntu 20.04 Lts/x64/Nodejs 10.19.0/Vim editor
+*/
+
+// program start
+
+console.log ("Hello, ES6 World!\nWelcome to ES6 programming!!\nLet us learn ES6 programming language.\n")
+
+// program exit
